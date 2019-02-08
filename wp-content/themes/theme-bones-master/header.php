2c81656b3e69a9e2de6169c1eee2f5c2439699f9
moranniall <moranniall@hotmail.com>
2019-02-08 11:07:14 +0000

    Added Logo
    Added Logo and repo test from mac to imac

--- wp-content/themes/theme-bones-master/header.php
+++ wp-content/themes/theme-bones-master/header.php
@@ -49,7 +49,7 @@
 				<div id="inner-header" class="wrap cf">
 
 					<?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
-					<p id="logo" class="h1" itemscope itemtype="http://schema.org/Organization"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
+					<a href="<?php echo home_url(); ?>" rel="nofollow"><img src="<?php echo get_stylesheet_directory_uri(); ?>/library/images/Logo.png"; ?></a>
 
 					<?php // if you'd like to use the site description you can un-comment it below ?>
 					<?php // bloginfo('description'); ?>
